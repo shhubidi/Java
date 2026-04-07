114ba18ee877a92b6dea2ecc6489fd78d371709e
Shubham <chitrakar05@gmail.com>
2026-04-07 15:29:11 +0530

    Updated code and added gitignore

--- Java2/MultipleCatchExample.java
+++ Java2/MultipleCatchExample.java
@@ -3,12 +3,13 @@ public class MultipleCatchExample{
         try{
             String str = null;
             System.out.println(str.length());
-        // 'catch(NullPointerException e){'
-        //     System.out.println("Error: null pointer exception!!");
+        catch(NullPointerException e){
+            System.out.println("Error: null pointer exception!!");
         }
         catch(Exception e){
             System.out.println("some other error");
         }
         System.out.println("program continues after exception handling.");
     }
+}
 }
